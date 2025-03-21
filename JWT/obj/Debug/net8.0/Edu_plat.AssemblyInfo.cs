fb66faeaec7fadbabdbdf7e3dd4622dacce1cb51
SALEH SHERIF <salehsherif016@gmail.com>
2025-03-21 15:11:18 +0200

    Edit Exam

--- JWT/obj/Debug/net8.0/Edu_plat.AssemblyInfo.cs
+++ JWT/obj/Debug/net8.0/Edu_plat.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Edu_plat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186372a9218d7490d6968f4ab9c63d6c8a28836c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea29f68c277e9f7ca8ebf9b0d8889ab4da90258")]
 [assembly: System.Reflection.AssemblyProductAttribute("Edu_plat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Edu_plat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
